f3ca6c5465b11fb7553d2738eb1c686dca8a768e
cat2koban <cat2koban@users.noreply.github.com>
2021-08-09 23:39:31 +0900

    refactor variable names

--- cat2koban/bin/setup.go
+++ cat2koban/bin/setup.go
@@ -7,15 +7,15 @@ import (
 )
 
 func main() {
-  contest_number := os.Args[1]
-  task_type := os.Args[2]
-  cmd_mkdir := exec.Command("mkdir", contest_number)
-  cmd_cp := exec.Command("cp", "template.go", contest_number+"/"+task_type+".go")
-  err_mkdir := cmd_mkdir.Run()
+  contest := os.Args[1]
+  task := os.Args[2]
+  mkdir := exec.Command("mkdir", contest)
+  cp := exec.Command("cp", "template.go", contest+"/"+task+".go")
+  err_mkdir := mkdir.Run()
   if err_mkdir != nil {
 		log.Fatal(err_mkdir)
 	}
-  err_cp := cmd_cp.Run()
+  err_cp := cp.Run()
   if err_cp != nil {
 		log.Fatal(err_cp)
 	}
